b831f4d9b0d712ba9c6d64a52eea7b00484bce8b
Thomas Rabaix <thomas.rabaix@gmail.com>
2017-10-24 14:02:32 +0200

    fix(legal): update date in header

--- errors.go
+++ errors.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Thomas Rabaix <thomas.rabaix@gmail.com>.
+// Copyright © 2016-present Thomas Rabaix <thomas.rabaix@gmail.com>.
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
